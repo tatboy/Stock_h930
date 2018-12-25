2e71d894eca7c8206f4517cdfbb820262eaf9e55
Brett Sharp <tattooboy12@gmail.com>
2018-12-25 08:12:20 -0800

    Revert "PM / devfreq: Remove null termination in boost_devices"
    This reverts commit 38ba31c3740ccc6e12a966905b0783a1d49be3c4.

--- drivers/devfreq/devfreq.c
+++ drivers/devfreq/devfreq.c
@@ -45,11 +45,7 @@ static DEFINE_MUTEX(devfreq_list_lock);
 
 /* List of devices to boost when the screen is woken */
 static const char *boost_devices[] = {
-<<<<<<< HEAD
 	NULL
-=======
-	"soc:qcom,cpubw"
->>>>>>> 8e8c96b... PM / devfreq: Remove null termination in boost_devices
 };
 
 #define WAKE_BOOST_DURATION_MS (10000)
@@ -1113,7 +1109,7 @@ static bool is_boost_device(struct devfreq *df)
 {
 	int i;
 
-	for (i = 0; ARRAY_SIZE(boost_devices); i++) {
+	for (i = 0; boost_devices[i]; i++) {
 		if (!strncmp(dev_name(&df->dev), boost_devices[i],
 				DEVFREQ_NAME_LEN))
 			return true;
